b86ee5968e9f8cf83b2434b4ba2229ed1f23e4ec
dmitriyLu <lubiannikovdmitriy@gmail.com>
2019-10-26 20:39:55 +0300

    added Mogo

---
+++ Mogo/index.js
@@ -0,0 +1,10 @@
+import js from './node_modules/@fortawesome/fontawesome-free/js/all.js'
+import './sass/base.sass'
+import query from './query'
+import greeting from './greet.js';
+import main from './js/main.js';
+import pageScroll from './js/scroll.js';
+
+// greeting('dima'); 
+
+
