dfa6eb140d1599864811dc759750ca9dae5edd60
dmitriyLu <lubiannikovdmitriy@gmail.com>
2019-10-29 17:11:49 +0200

    commit beetle

---
+++ Beetle/js/owl.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $(".designers").owlCarousel({
+        loop:true,
+        items:1,
+        responsiveClass:true,
+        animateOut: 'fadeOut',
+        margin: 10,
+        autoplay: true,
+        dots: true,
+        
+    });
+  });
