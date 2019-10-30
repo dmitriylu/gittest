78b0a0a6159604bb870e6196693022ba518336cf
dmitriyLu <lubiannikovdmitriy@gmail.com>
2019-10-30 17:47:34 +0200

    commit

---
+++ granit/src/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('.your-class').slick();
+  });
+
+sum(4,6);
+clickB();
+burgerClick();
+hideScrollTop();
+smoothScroll();
+
+// <svg class="icon">
+            //     <use xlink:href="./build/img/svg/symbol/sprite.svg#logo"></use>
+            // </svg>
+           
